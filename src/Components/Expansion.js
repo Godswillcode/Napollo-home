c81e089d4736135a8d29ce046cf4e611b1d70404
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 23:34:05 +0100

    working on expansion page

--- src/Components/Expansion.js
+++ src/Components/Expansion.js
@@ -1,13 +1,15 @@
+import expan from "../Images/expan.jpg"
+
 const Expansion = () => {
   return (
-    <div className="myContainer">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-        <img src="" alt="" />
+    <div className="myContainer mt-44">
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        <img src={expan} alt="Expansion" className="h-2/3"/>
 
         {/* detail */}
         <div>
           <h2 className="title">Expansion Page</h2>
-          <p>
+          <p className="text-lg font-medium">
             Discover artistes all around the world by their location via the
             expansion page. Also, get to know the top artistes in any location
             on the globe.
