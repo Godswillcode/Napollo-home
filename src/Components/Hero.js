97dd27e2435fd2f301b467769f407dbc8cc422d3
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 15:29:08 +0100

    setting up

---
+++ src/Components/Hero.js
@@ -0,0 +1,17 @@
+import heroBg from "../Images/heroImage.jpg"
+
+const Hero = () => {
+    return (
+        <div className="myContainer">
+             <div className="grid grid-cols-2">
+                  <div>
+
+                  </div>
+
+                  <img src={heroBg} alt="header" />
+             </div>
+        </div>
+    )
+}
+
+export default Hero
