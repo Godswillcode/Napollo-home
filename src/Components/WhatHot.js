863a08482796ccf1a1667de9e2642a0519e8a3e7
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 22:42:33 +0100

    working on home

--- src/Components/WhatHot.js
+++ src/Components/WhatHot.js
@@ -16,8 +16,17 @@ const WhatHot = () => {
 
                      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                          <div>
-                             <img src={album1} alt="album" />
-
+                             <img src={album1} alt="album" className="rounded-md"/>
+                                <div className="mt-2 flex justify-between text-sm">
+                                    <div>
+                                        <span className="block text-neutral">Death Bed</span>
+                                        <span className="text-primary">Powerful </span>
+                                    </div>
+                                    <div>
+                                        <span><svg className="w-6 h-6 text-neutral" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg></span>
+                                        <span className="block text-neutral">1.3k</span>
+                                    </div>
+                                </div>
                          </div>
                      </div>
                 </div>
