ac1aadd5f1450cb0a3838c6fe8d2e262aee19f33
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 19:38:32 +0100

    working on home

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,14 +5,16 @@ const Hero = () => {
     <div className="myContainer">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
         <div className="pt-20">
-          <h3 className="text-white text-5xl font-black leading-tight">
+          <h3 className="text-white text-2xl md:text-5xl font-black md:leading-tight">
             More fans, More Gigs, Less Eort Get better feedback, wider exposure
             and deeper industry access without ever switching tabs.
           </h3>
-          <button className="border rounded-3xl text-white px-4 py-2 text-sm mt-8">Let's get Started</button>
+          <button className="border rounded-3xl text-white px-4 py-2 text-sm mt-8">
+            Let's get Started
+          </button>
         </div>
 
-        <img src={heroBg} alt="header" style={{height: "80%"}}/>
+        <img src={heroBg} alt="header" className="md:h-4/5" />
       </div>
     </div>
   );
