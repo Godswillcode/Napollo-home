eed16ba80ba0f21db1e04ca5e4d29757d3f70d3d
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 19:21:35 +0100

    working on home

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,7 +3,7 @@ import heroBg from "../Images/heroImage.jpg";
 const Hero = () => {
   return (
     <div className="myContainer">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
         <div className="pt-20">
           <h3 className="text-white text-4xl font-bold">
             More fans, More Gigs, Less Eort Get better feedback, wider exposure
@@ -12,7 +12,7 @@ const Hero = () => {
           <button className="border rounded-3xl text-white px-4 py-2 text-sm mt-4">Let's get Started</button>
         </div>
 
-        <img src={heroBg} alt="header" />
+        <img src={heroBg} alt="header" height="50%" width="80%"/>
       </div>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import logo from "../Images/logo.png";
+import apple from "../Images/apple.png"
+import google from "../Images/googlePlay.png"
 
 const Navbar = () => {
   return (
-    <div className="shadow-xl head py-1">
+    <nav className="shadow-xl nav py-1">
       <div className="myContainer ">
         <div className="flex items-center">
           <div className="flex items-center">
@@ -24,12 +26,12 @@ const Navbar = () => {
 
             <img src={logo} alt="logo" className="w-16" />
           </div>
-          <div className="">
-            <div class="col-rt-3 equal-height">
-              <div class="sb-example-3">
-                <div class="search__container">
+          <div className="ml-8">
+            <div className="col-rt-3 equal-height">
+              <div className="sb-example-3">
+                <div className="search__container">
                   <input
-                    class="search__input"
+                    className="search__input"
                     type="text"
                     placeholder="Search for artistes, songs, albums"
                   />
@@ -37,9 +39,18 @@ const Navbar = () => {
               </div>
             </div>
           </div>
+
+          <div className="flex items-center ml-32 justify-center">
+              <img src={apple} alt="apple" className="w-32"/>
+              <img src={google} alt="google playstore" className="w-32"/>
+              <a href="#!" className="text-white mx-3">SignUp/Login</a>
+
+             <label htmlFor="upload" className="flex items-center text-white border border-secondary py-1 px-6 rounded-2xl" style={{background: "linear-gradient(to top, #f78127, #feed3e)"}}><svg width="22" height="22" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M9 16h-8v6h22v-6h-8v-1h9v8h-24v-8h9v1zm11 2c.552 0 1 .448 1 1s-.448 1-1 1-1-.448-1-1 .448-1 1-1zm-7.5 0h-1v-14.883l-4.735 5.732-.765-.644 6.021-7.205 5.979 7.195-.764.645-4.736-5.724v14.884z" fill="#fff"/></svg>  <span className="pl-3">Upload</span></label>
+              <input type="file" name="file" id="upload" className="hidden"/>
+          </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
