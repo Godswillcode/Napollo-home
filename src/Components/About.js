7fd77dc6b924593ca2ea587e241ad668ef1e5c4e
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 19:57:37 +0100

    working on home

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,20 +3,20 @@ import logo from "../Images/logo.png";
 const About = () => {
   return (
     <div className="myContainer">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div>
-          <h2>About Napollo</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-white">
+        <div className="pt-40 md:pr-8">
+          <h2 className="text-primary text-2xl md:text-5xl font-extrabold pb-5">About Napollo</h2>
           <p>
             Napollo is a music streaming service that connects artistes to
             listeners in a way that has never been done before.
           </p>
-          <p>
+          <p className="py-5">
             Artists, musicians, music lovers, producers, no matter your taste or
             skills are, we are here to serve you the best all around the world.
           </p>
         </div>
 
-        <img src="" alt="" />
+        <img src={logo} alt="about Napollo" className=""/>
       </div>
     </div>
   );
