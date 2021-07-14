7e41ef631986289238b1515805a1fec001629eae
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 19:46:15 +0100

    working on home

---
+++ src/Components/About.js
@@ -0,0 +1,25 @@
+import logo from "../Images/logo.png";
+
+const About = () => {
+  return (
+    <div className="myContainer">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <h2>About Napollo</h2>
+          <p>
+            Napollo is a music streaming service that connects artistes to
+            listeners in a way that has never been done before.
+          </p>
+          <p>
+            Artists, musicians, music lovers, producers, no matter your taste or
+            skills are, we are here to serve you the best all around the world.
+          </p>
+        </div>
+
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default About;
