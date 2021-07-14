9dd5eb915b817bfb96d2655501ac411fbfc4224f
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 23:26:51 +0100

    working on expansion page

--- src/Components/Expansion.js
+++ src/Components/Expansion.js
@@ -1,11 +1,21 @@
-
-
 const Expansion = () => {
-    return (
-        <div className="myContainer">
-            
-        </div>
-    )
-}
+  return (
+    <div className="myContainer">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+        <img src="" alt="" />
 
-export default Expansion
+        {/* detail */}
+        <div>
+          <h2 className="title">Expansion Page</h2>
+          <p>
+            Discover artistes all around the world by their location via the
+            expansion page. Also, get to know the top artistes in any location
+            on the globe.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Expansion;
