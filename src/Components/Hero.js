9770e8bcf4d91eed56b0c577303dbba1c0dc1ece
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-14 17:33:15 +0100

    setting up

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,17 +1,21 @@
-import heroBg from "../Images/heroImage.jpg"
+import heroBg from "../Images/heroImage.jpg";
 
 const Hero = () => {
-    return (
-        <div className="myContainer">
-             <div className="grid grid-cols-2">
-                  <div>
-
-                  </div>
-
-                  <img src={heroBg} alt="header" />
-             </div>
+  return (
+    <div className="myContainer">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+        <div className="pt-20">
+          <h3 className="text-white text-2xl font-bold">
+            More fans, More Gigs, Less Eort Get better feedback, wider exposure
+            and deeper industry access without ever switching tabs.
+          </h3>
+          <button className="border rounded-3xl text-white px-4 py-2 text-sm mt-4">Let's get Started</button>
         </div>
-    )
-}
 
-export default Hero
+        <img src={heroBg} alt="header" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import logo from "../Images/logo.png"
 
 const Navbar = () => {
     return (
-        <div className="">
-            
+         <div className="bg-secondary">
+             <div className="myContainer shadow-sm">
+            <div className="wrap">
+                <div className="flex items-center child1">
+                <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+
+                <img src={logo} alt="logo" className="w-16"/>
+                </div>
+                <div className="child2">
+                    <input type="search" name="" id="" />
+                </div>
+            </div>
         </div>
+         </div>
     )
 }
 
