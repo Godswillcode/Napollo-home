64c4e117e6aec4483d4b2597cbc7dc9c88e479be
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-15 19:40:30 +0100

    working body

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
       <Expansion />
       <Listen />
       <Plan />
-      {/* <Testimonial /> */}
-      {/* <Download /> */}
-      {/* <Footer /> */}
+      <Testimonial />
+      <Download />
+      <Footer />
       <div className="mb-60"></div>
     </>
   );
--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -7,73 +7,45 @@ const Testimonial = () => {
         <h2 className="title text-center">Testimonials</h2>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-x-16">
-
-      <div>
-      <div className="bg-secondary py-10 pr-7 rounded-br-lg rounded-tl">
-        <p>This is the best app I've come across.
-          was able to upscale my career in less
-          than 10 weeks.</p>
-        <div className="text-right pr-10 pt-5 font-semibold tracking-wider text-sm">
-          <h3 className="text-neutral">Hanna Gosley</h3>
-          <h4 className="text-primary">Pop Artiste</h4>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 md:gap-x-16">
+        <div className="hidden lg:flex">
+          <div>
+          <div className="bg-secondary py-10 pr-7 rounded-br-lg rounded-tl">
+            <p>
+              This is the best app I've come across. was able to upscale my
+              career in less than 10 weeks.
+            </p>
+            <div className="text-right pr-10 pt-5 font-semibold tracking-wider text-sm">
+              <h3 className="text-neutral">Hanna Gosley</h3>
+              <h4 className="text-primary">Pop Artiste</h4>
+            </div>
+          </div>
+          <div className="flex justify-end -mt-8">
+            <img src={testImage} alt="testimonials" className="rounded-full" />
+          </div>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-end -mt-8">
-        <img src={testImage} alt="testimonials" className="rounded-full" />
-      </div>
-    </div>
 
-    <div>
-      <div className="bg-secondary py-10 px-7 rounded-br-lg rounded-tl">
-        <p>Beign an artiste on Napollo has been a real game changer.</p>
-        <div className="text-right pr-10 pt-5 font-semibold tracking-wider text-sm">
-          <h3 className="text-neutral">Hanna Gosley</h3>
-          <h4 className="text-primary">Pop Artiste</h4>
+        <div>
+          <div className="bg-secondary py-10 px-7 rounded-br-lg rounded-tl mx-3">
+            <p>Beign an artiste on Napollo has been a real game changer.</p>
+            <div className="text-right pr-10 pt-5 font-semibold tracking-wider text-sm">
+              <h3 className="text-neutral">Hanna Gosley</h3>
+              <h4 className="text-primary">Pop Artiste</h4>
+            </div>
+          </div>
+          <div className="flex justify-end -mt-8">
+            <img src={testImage} alt="testimonials" className="rounded-full" />
+          </div>
         </div>
-      </div>
-      <div className="flex justify-end -mt-8">
-        <img src={testImage} alt="testimonials" className="rounded-full" />
-      </div>
-    </div>
 
-    <div>
-      <div className="bg-secondary py-16 px-14 rounded-br-lg rounded-tl">
-        <p className="py-1">Beign an artiste on Napollo has been a real game changer.</p>
-        
-      </div>
-      
-    </div>
-        
-
-
-        {/* <div className="">
-          <TItems
-            desc="Napollo is such a great platform. I'm
-          getting exposure and opportunities that"
-            image={testImage}
-            name="Hanna Gosley"
-            job="Pop Artiste"
-          />
-        </div> */}
-      </div>
-    </div>
-  );
-};
-
-// Testimonial items
-const TItems = ({ desc, image, name, job }) => {
-  return (
-    <div className="">
-      <div className="bg-secondary py-10 px-7 rounded-br-lg rounded-tl">
-        <p>{desc}</p>
-        <div className="text-right pr-10 pt-5 font-semibold tracking-wider text-sm">
-          <h3 className="text-neutral">{name}</h3>
-          <h4 className="text-primary">{job}</h4>
+        <div className="hidden lg:block">
+          <div className="bg-secondary py-16 px-14 rounded-br-lg rounded-tl">
+            <p className="py-1">
+              Beign an artiste on Napollo has been a real game changer.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-end -mt-8">
-        <img src={image} alt="testimonials" className="rounded-full" />
       </div>
     </div>
   );
