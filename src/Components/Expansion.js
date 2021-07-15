e75b705b57f1eab3765c140fbf13c49a353d0e63
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-15 10:57:56 +0100

    working on expansion page

--- src/Components/Expansion.js
+++ src/Components/Expansion.js
@@ -20,12 +20,12 @@ const Expansion = () => {
           </p>
 
           <div className="mt-10">
-              <ChildComp number="01" image={ex1} title1="Lights Out" title2="ColdPlay" icon="#fff"/>
+              <ChildComp number="01" image={ex1} title1="Lights Out" title2="ColdPlay" icon="#3a3a3a"/>
            
-              <ChildComp number="02" image={ex2} title2="ColdPlay" icon="#fff"/>
+              <ChildComp number="02" image={ex2} title2="Green Tuska" icon="#f78127"/>
               
-              <ChildComp number="03" image={ex3}  title2="ColdPlay" icon="#fff"/>
-              <ChildComp number="04" image={ex4}  title2="ColdPlay" icon="#fff"/>
+              <ChildComp number="03" image={ex3}  title2="Bruno Mars" icon="#3a3a3a"/>
+              <ChildComp number="04" image={ex4}  title2="Cardi" icon="#3a3a3a"/>
           </div>
         </div>
       </div>
@@ -38,15 +38,18 @@ const Expansion = () => {
 
 const ChildComp = ({number, image, title1, title2, icon}) => {
     return(
-        <div className="flex items-center mt-5">
-            <h4 className="text-white pr-3">{number}</h4>
+        <div className="flex items-center justify-self-stretch mt-5">
+            <h4 className="text-white pr-3 font-bold">{number}</h4>
              <img src={image} alt="expansion" className="rounded-md mr-4"/>
              <div>
                   <span className="block text-neutral text-sm">{title1}</span>
-                  <span className="text-secondary font-bold text-sm">{title2}</span>
+                  <span className="text-gray-600 font-medium text-sm">{title2}</span>
              </div>
-                 <div className="ml-20">
-                 <svg className="w-7 h-7" fill={icon} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                 
+                 <div className="">
+                <div>
+                <svg className="w-7 h-7" fill={icon} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                </div>
                  </div>
         </div>
     )
