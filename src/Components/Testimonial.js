c2f25dda62c46467742a63887ec1b7d9c7ef8f88
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-15 13:01:35 +0100

    working on testimonial section

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -2,7 +2,21 @@
 const Testimonial = () => {
     return (
         <div>
-            test
+            <div className="myContainer">
+                 <h2 className="title">Testimonials</h2>
+            </div>
+
+              <Testimonial desc="This is the best app I've come across. was able to upscale my career in lessthan 10 weeks." name="Godswill hope" job="devloper"/>
+        </div>
+    )
+}
+
+
+// Testimonial items
+const TItems = ({desc}) => {
+    return (
+        <div className="bg-secondary">
+              <p>{desc}</p>
         </div>
     )
 }
