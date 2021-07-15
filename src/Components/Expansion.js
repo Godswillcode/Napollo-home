57063cf3832dd23cecffa13a3daa27f0af908c86
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-15 11:14:28 +0100

    working on expansion page

--- src/Components/Expansion.js
+++ src/Components/Expansion.js
@@ -1,14 +1,14 @@
-import expan from "../Images/expan.jpg"
-import ex1 from "../Images/ex1.png"
-import ex2 from "../Images/ex2.png"
-import ex3 from "../Images/ex3.png"
-import ex4 from "../Images/ex4.png"
+import expan from "../Images/expan.jpg";
+import ex1 from "../Images/ex1.png";
+import ex2 from "../Images/ex2.png";
+import ex3 from "../Images/ex3.png";
+import ex4 from "../Images/ex4.png";
 
 const Expansion = () => {
   return (
     <div className="myContainer mt-44">
       <div className="grid grid-cols-1 md:grid-cols-2">
-        <img src={expan} alt="Expansion" className="h-2/3"/>
+        <img src={expan} alt="Expansion" className="h-2/3" />
 
         {/* detail */}
         <div>
@@ -20,12 +20,34 @@ const Expansion = () => {
           </p>
 
           <div className="mt-10">
-              <ChildComp number="01" image={ex1} title1="Lights Out" title2="ColdPlay" icon="#3a3a3a"/>
-           
-              <ChildComp number="02" image={ex2} title2="Green Tuska" icon="#f78127"/>
-              
-              <ChildComp number="03" image={ex3}  title2="Bruno Mars" icon="#3a3a3a"/>
-              <ChildComp number="04" image={ex4}  title2="Cardi" icon="#3a3a3a"/>
+            <ChildComp
+              number="01"
+              image={ex1}
+              title1="Lights Out"
+              title2="ColdPlay"
+              icon="#3a3a3a"
+            />
+
+            <ChildComp
+              number="02"
+              image={ex2}
+              title2="Green Tuska"
+              icon="#f78127"
+            />
+
+            <ChildComp
+              number="03"
+              image={ex3}
+              title2="Bruno Mars"
+              icon="#3a3a3a"
+            />
+            <ChildComp
+              number="04"
+              image={ex4}
+              title2="Cardi"
+              icon="#3a3a3a"
+              hiddenT="........."
+            />
           </div>
         </div>
       </div>
@@ -33,26 +55,39 @@ const Expansion = () => {
   );
 };
 
-
 // Child component
 
-const ChildComp = ({number, image, title1, title2, icon}) => {
-    return(
-        <div className="flex items-center justify-self-stretch mt-5">
-            <h4 className="text-white pr-3 font-bold">{number}</h4>
-             <img src={image} alt="expansion" className="rounded-md mr-4"/>
-             <div>
-                  <span className="block text-neutral text-sm">{title1}</span>
-                  <span className="text-gray-600 font-medium text-sm">{title2}</span>
-             </div>
-                 
-                 <div className="">
-                <div>
-                <svg className="w-7 h-7" fill={icon} stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
-                </div>
-                 </div>
+const ChildComp = ({ number, image, title1, title2, icon, hiddenT }) => {
+  return (
+    <div className="flex items-center justify-around mt-5 md:mr-80">
+      <div className="flex items-center">
+        <h4 className="text-white pr-3 font-bold">{number}</h4>
+        <img src={image} alt="expansion" className="rounded-md mr-4" />
+        <div className="">
+          <span className="block text-neutral text-sm">{title1}</span>
+          <span className="text-gray-600 font-medium text-sm">
+            {title2} <i className="invisible">{hiddenT}</i>{" "}
+          </span>
         </div>
-    )
-}
+      </div>
+      <div className="ml-10">
+        <svg
+          className="w-7 h-7"
+          fill={icon}
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
 
 export default Expansion;
--- src/Components/Listen.js
+++ src/Components/Listen.js
@@ -1,21 +1,28 @@
-import catset from "../Images/catset.png"
+import catset from "../Images/catset.png";
 
 const Listen = () => {
   return (
-    <div className="bg-primary py-14 px-5">
+    <div className="bg-primary py-14 px-5 mt-12 md:mt-0">
       <div className="myContainer grid grid-cols-1 md:grid-cols-2 gap-4">
         <img src={catset} alt="listen" />
         <div>
-          <h2 className="text-2xl md:text-6xl font-extrabold pb-5 text-black">Listen and enjoy all trending music</h2>
+          <h2 className="text-2xl md:text-6xl font-extrabold pb-5 text-black">
+            Listen and enjoy all trending music
+          </h2>
 
           <ul>
-              <li>What’s hot</li>
-              <li>By genre</li>
-              <li>By location</li>
-              <li>Lot’s more...</li>
+            <li>What’s hot</li>
+            <li>By genre</li>
+            <li>By location</li>
+            <li>Lot’s more...</li>
           </ul>
 
-          <button className="rounded-2xl px-5 mt-4 py-1 text-primary text-sm" style={{background: "rgba(0,0,0,255)"}}>Start listening</button>
+          <button
+            className="rounded-2xl px-5 mt-4 py-1 text-primary text-sm"
+            style={{ background: "rgba(0,0,0,255)" }}
+          >
+            Start listening
+          </button>
         </div>
       </div>
     </div>
