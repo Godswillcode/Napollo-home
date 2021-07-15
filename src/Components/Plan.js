126c467bdf0614084d3069a50293737a64060cdd
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-15 11:36:06 +0100

    working on plan section

--- src/Components/Plan.js
+++ src/Components/Plan.js
@@ -1,4 +1,5 @@
-
+import logoW from "../Images/logo2.png"
+import logo from "../Images/logo.png"
 
 const Plan = () => {
     return (
@@ -6,8 +7,18 @@ const Plan = () => {
              <h2 className="title text-center">Choose Your Plan</h2>
 
              <div className="flex justify-center">
-                 <div>
-                  
+                 <div className="bg-primary py-5 px-10 border">
+                     <div className="flex justify-center items-center p-2 rounded-full">
+                     <img src={logoW} alt="logo"  className="rounded-full" className="w-28 h-28"/>
+                     </div>
+
+                     <div className="text-center text-white">
+                         <h4 className="font-bold tracking-wider pb-1">Free</h4>
+                         <p className="text-sm text-white">Enjoy unlimited online music</p>
+                          <div className="flex justify-center mt-3">
+                          <div className="bg-white w-12 h-1"></div>
+                          </div>
+                     </div>
                  </div>
              </div>
         </div>
